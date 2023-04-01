54059d9eea7a93502aa07a528a945dfa5ed1202e
lahiruSandaruwan20 <lahiru.20210390@iit.ac.lk>
2023-04-01 21:57:05 +0530

    Fixed sending data as json objects

--- dummy/testing.py
+++ dummy/testing.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__)
 
@@ -11,27 +11,44 @@ def getShirtList(name):
     model = keras.models.load_model('keras_model.h5')
     # define the class labels
     class_labels = ['Short-Sleeve Shirt', 'Long-Sleeve Shirt', 'Skinny']
-    # num_displayed = 0
-    img = keras.preprocessing.image.load_img('tshirt/2.jpg'.format(2), target_size=(224, 224))
-    img_array = keras.preprocessing.image.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0)
-    img_array = keras.applications.mobilenet_v2.preprocess_input(img_array)
+    img_array_list = []
+    
+    for i in range(1, 6):
+        # num_displayed = 0
+        img = keras.preprocessing.image.load_img('tshirt/{}.jpg'.format(i), target_size=(224, 224))
+        img_array = keras.preprocessing.image.img_to_array(img)
+        img_array = tf.expand_dims(img_array, 0)
+        img_array = keras.applications.mobilenet_v2.preprocess_input(img_array)
 
-    # make predictions on the test image
-    predictions = model.predict(img_array)
-    predicted_class = class_labels[tf.argmax(predictions, axis=1)[0]]
+        # make predictions on the test image
+        predictions = model.predict(img_array)
+        predicted_class = class_labels[tf.argmax(predictions, axis=1)[0]]
 
-    if predicted_class == name:
+        if predicted_class == "Long-Sleeve Shirt" == name:
+            print(predicted_class)
+            img_array_list.append(img_array.numpy().tolist())
+            return jsonify(img_array_list)
+            # return name
 
-        # img.show()
-        # num_displayed += 1
-        return name
+        elif "Short-Sleeve Shirt" == name:
+            short_sleeve_array_list = []
 
-    # exit the loop after displaying 7 Long-Sleeve Shirt images
-    if predicted_class != name:
-        # num_displayed += 1
-        print('xxx')
-        return name
+            for i in range(1, 6):
+                # load the image and preprocess it
+                img = keras.preprocessing.image.load_img(f'shortsleeve/{i}.jpg', target_size=(224, 224))
+                img_array = keras.preprocessing.image.img_to_array(img)
+                img_array = tf.expand_dims(img_array, 0)
+                img_array = keras.applications.mobilenet_v2.preprocess_input(img_array)
+                short_sleeve_array_list.append(img_array.numpy().tolist())
+
+                return jsonify(short_sleeve_array_list)
+
+        
+        # # exit the loop after displaying 7 Long-Sleeve Shirt images
+        # if predicted_class != name:
+        #     # num_displayed += 1
+        #     print('xxx')
+        #     return name
 
 
 
