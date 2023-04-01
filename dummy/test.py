6232b26497f9b1ffa92915d694bbda62d76d145c
lahiruSandaruwan20 <lahiru.20210390@iit.ac.lk>
2023-04-01 17:49:12 +0530

    import Flask and wrote a simple API to test GET method

--- dummy/test.py
+++ dummy/test.py
@@ -15,7 +15,7 @@ class_labels = ['Short-Sleeve Shirt', 'Long-Sleeve Shirt', 'Skinny']
 num_displayed = 0
 
 #load the test image and preprocess it
-for i in range(1, 51):
+for i in range(1, 20):
     img = keras.preprocessing.image.load_img('tshirt/{}.jpg'.format(i), target_size=(224, 224))
     img_array = keras.preprocessing.image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)
@@ -24,7 +24,7 @@ for i in range(1, 51):
     # make predictions on the test image
     predictions = model.predict(img_array)
     predicted_class = class_labels[tf.argmax(predictions, axis=1)[0]]
-    
+
 
     # print the predicted class
     print('Predicted class:', predicted_class)
@@ -43,4 +43,5 @@ for i in range(1, 51):
 
             # exit the loop after displaying 7 Long-Sleeve Shirt images
     if predicted_class == "Short-Sleeve Shirt":
+        num_displayed += 1
         print('xxx')
