56b2e60b2721a45829cda095450d694cbfbee8a3
lahiruSandaruwan20 <lahiru.20210390@iit.ac.lk>
2023-04-05 01:55:52 +0530

    modified if else for testing Long and short sleeve shirts

--- dummy/testing.py
+++ dummy/testing.py
@@ -12,8 +12,9 @@ def getShirtList(name):
     class_labels = ['Short-Sleeve Shirt', 'Long-Sleeve Shirt', 'Skinny']
     img_array_list = []
     img_name_list = []
+    # img_name_list_short = []
     
-    for i in range(1, 30):
+    for i in range(1, 50):
         # num_displayed = 0
         img = keras.preprocessing.image.load_img(f'tshirt/{i}.jpg', target_size=(224, 224))
         img_array = keras.preprocessing.image.img_to_array(img)
@@ -23,27 +24,19 @@ def getShirtList(name):
         # make predictions on the test image
         predictions = model.predict(img_array)
         predicted_class = class_labels[tf.argmax(predictions, axis=1)[0]]
-        img_name_list.append(f'{i}.jpg')
+        # img_name_list.append(f'{i}.jpg')
 
-        if predicted_class == "Long-Sleeve Shirt" == name:
+        if  predicted_class == "Long-Sleeve Shirt" and name == "Long-Sleeve Shirt":
             print(predicted_class)
-            img_array_list.append(img_array.numpy().tolist())
-            # return jsonify(img_array_list)
-            return jsonify({'img_name_list': img_name_list})
-            # return name
+            img_name_list.append(f'{i}.jpg')
+            
 
-        elif "Short-Sleeve Shirt" == name:
-            short_sleeve_array_list = []
-
-            for i in range(1, 30):
-                # load the image and preprocess it
-                img = keras.preprocessing.image.load_img(f'tshirt/{i}.jpg', target_size=(224, 224))
-                img_array = keras.preprocessing.image.img_to_array(img)
-                img_array = tf.expand_dims(img_array, 0)
-                img_array = keras.applications.mobilenet_v2.preprocess_input(img_array)
-                short_sleeve_array_list.append(img_array.numpy().tolist())
-
-                return jsonify({'img_name_list': img_name_list})
+        elif predicted_class == "Short-Sleeve Shirt" and name == "Short-Sleeve Shirt":
+            print(predicted_class)
+            img_name_list.append(f'{i}.jpg')
+            
+        
+    return jsonify({'img_name_list': img_name_list})
 
 if __name__ == '__main__':
     app.run(debug=True)
