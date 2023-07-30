b6c48fc16f13ce6d75d811dda7f27d93ecc9caae
lahiruSandaruwan20 <lahiru.20210390@iit.ac.lk>
2023-07-31 01:02:14 +0530

    Connect to mysql DB

--- dummy/testing.py
+++ dummy/testing.py
@@ -1,8 +1,27 @@
 import tensorflow as tf
+import MySQLdb.cursors
 from tensorflow import keras
 from flask import Flask,jsonify
+from flask_mysqldb import MySQL
+from MySQLdb.cursors import DictCursor
+
+
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_DB'] = 'aera'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_PORT'] = '3306'  # Specify the port as a string
+
+mysql = MySQLdb.connect(user=app.config['MYSQL_USER'],
+                        passwd=app.config['MYSQL_PASSWORD'],
+                        db=app.config['MYSQL_DB'],
+                        host=app.config['MYSQL_HOST'],
+                        port=int(app.config['MYSQL_PORT']))
 
 @app.route('/edit/<string:name>', methods=['GET'])
 def getShirtList(name):
@@ -10,12 +29,10 @@ def getShirtList(name):
     model = keras.models.load_model('keras_model.h5')
     # define the class labels
     class_labels = ['Short-Sleeve Shirt', 'Long-Sleeve Shirt', 'Skinny']
-    img_array_list = []
     img_name_list = []
-    # img_name_list_short = []
+
     
     for i in range(1, 8):
-        # num_displayed = 0
         img = keras.preprocessing.image.load_img(f'tshirt/{i}.png', target_size=(224, 224))
         img_array = keras.preprocessing.image.img_to_array(img)
         img_array = tf.expand_dims(img_array, 0)
@@ -33,9 +50,27 @@ def getShirtList(name):
             print(predicted_class)
             img_name_list.append(f'{i}.png')
             
+            
         
     return jsonify({'img_name_list': img_name_list})
 
+
+@app.route('/type/<int:product_id>', methods=['GET'])
+def getType(product_id):
+    
+    cursor = mysql.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT product_no,product_type,price FROM product WHERE id = %s", (product_id,))
+    product = cursor.fetchone()
+    return jsonify(product)
+   
+    
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
 
